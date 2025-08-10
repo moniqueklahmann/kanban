a7d40e25f09af36f7d61094cb9fa61e77cc83c7a
moniqueklahmann <moniqueklahmannsec@gmail.com>
2025-08-10 15:00:08 -0300

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    dragula([
+    document.getElementById('todo'),
+    document.getElementById('in-progress'),
+    document.getElementById('done')
+    ]);
+});
+
